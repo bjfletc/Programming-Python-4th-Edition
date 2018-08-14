276f78c0f3752fa3e7b5631c63061d5de7903548
Brandon Fletcher <brandonjfletcher@gmail.com>
2018-08-14 12:37:10 -0500

    'people-pickle' as to not overwrite our own data

--- Chapter 1, A Sneak Preview/make_db_pickle.py
+++ Chapter 1, A Sneak Preview/make_db_pickle.py
@@ -1,6 +1,6 @@
 from initdata import db
 import pickle
 
-dbfile = open('people-file', 'wb')  # # use binary mode files in 3.X
+dbfile = open('people-pickle', 'wb')  # # use binary mode files in 3.X
 pickle.dump(db, dbfile)
 dbfile.close()
